cae21f5d4f7468f95ddb654c8916325d22d6d0a5
Steven Lopez <cslopezmontano@gmail.com>
2024-05-09 23:20:03 -0500

    #OO - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Stevenxyn.java
@@ -0,0 +1,36 @@
+// Sitio oficial https://www.java.com/es/
+
+// Comentario de una linea
+
+/*
+Comentario
+de
+varias
+lineas.
+*/
+
+public class Stevenxyn {
+  public static void main (String[] args){
+    
+    // Variable normal
+    int year = 1999;
+    // Constante
+    final String myUserGit = "Stevenxyn";
+
+    //Tipos de datos primitivos
+    boolean isActive = true;
+    boolean isActive2 = false;
+
+    byte age = 122;
+    short latitudee = 12616;
+    char characterr = 'a';
+    int number = 150362;
+    float flotante = 1234.11f;
+    long valueX = 212421l;
+    double valueY = 9812891.9291;
+
+    String languageName = "It's JAVA BABY!";
+    System.out.println("Welcome, " + languageName);
+
+  }
+}
